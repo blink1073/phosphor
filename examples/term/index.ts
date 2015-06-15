3df3224b3f63636e39e4e14a26b697e873d1a1f2
Steven Silvester <steven.silvester@ieee.org>
2015-06-15 13:47:39 -0500

    Add resize capability and terminal config

--- examples/term/index.ts
+++ examples/term/index.ts
@@ -13,25 +13,31 @@ import ResizeMessage = phosphor.widgets.ResizeMessage;
 import Widget = phosphor.widgets.Widget;
 
 
-interface ISize {
-  rows: number;
-  cols: number;
+interface ITerminalConfig {
+  convertEol?: boolean;
+  termName?: string;
+  rows?: number;
+  cols?: number;
+  cursorBlink?: boolean;
+  visualBell?: boolean;
+  popOnBell?: boolean;
+  scrollback?: number;
+  screenKeys?: number;
+  useStyle?: boolean;
 }
+
+
+declare function Terminal(config: ITerminalConfig): void;
 
 
 class TermWidget extends Widget {
 
-  constructor(size: ISize, ws_url: string) {
+  constructor(ws_url: string, config?: ITerminalConfig) {
     super();
     this.addClass('TermWidget');
     this._ws = new WebSocket(ws_url);
-    this._term = new Terminal({
-      cols: size.cols,
-      rows: size.rows,
-      screenKeys: true,
-      useStyle: true
-    });
 
+    this._term = Terminal(config || { useStyle: true });
     this._term.open(this.node);
 
     this._term.on('data', (data: string) => {
@@ -59,7 +65,13 @@ class TermWidget extends Widget {
   }
 
   protected onResize(msg: ResizeMessage): void {
-    //this._editor.setSize(msg.width, msg.height);
+    var termRowHeight = this._term.element.offsetHeight / this._term.rows;
+    var termColWidth = this._term.element.offsetWidth / this._term.cols;
+
+    var rows = Math.max(2, Math.floor(msg.height / termRowHeight) - 1);
+    var cols = Math.max(3, Math.floor(msg.width / termColWidth) - 1);
+
+    this._term.resize(cols, rows);
   }
 
   private _ws: WebSocket;
@@ -72,10 +84,12 @@ function main(): void {
   var protocol = (window.location.protocol.indexOf("https") === 0) ? "wss" : "ws";
   var ws_url = protocol + "://" + window.location.host + "/websocket";
 
-  var term = new TermWidget({rows: 40, cols: 80}, ws_url);
+  var term = new TermWidget(ws_url);
 
   term.attach(document.getElementById('main'));
   term.fit();
+
+  window.onresize = () => term.fit();
 
 }
 
