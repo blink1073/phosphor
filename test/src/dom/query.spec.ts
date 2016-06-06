4cd1ef64ad16d3dd29a956bde963e1ccbba71f3a
Steven Silvester <steven.silvester@ieee.org>
2016-06-06 14:05:19 -0400

    Finish query tests

--- test/src/dom/query.spec.ts
+++ test/src/dom/query.spec.ts
@@ -8,7 +8,7 @@
 import expect = require('expect.js');
 
 import {
-  hitTest
+  hitTest, scrollIfNeeded
 } from '../../../lib/dom/query';
 
 
@@ -44,6 +44,107 @@ describe('dom/query', () => {
       expect(hitTest(div, 100, 100)).to.be(false);
       expect(hitTest(div, 99, 99)).to.be(true);
       document.body.removeChild(div);
+    });
+
+  });
+
+  describe('scrollIfNeeded()', () => {
+
+    it('should scroll an element up if needed', () => {
+      let area = document.createElement('div');
+      let elem = document.createElement('div');
+      area.style.height = '100px';
+      area.style.overflow = 'auto';
+      elem.textContent = 'visible content';
+      for (let i = 0; i < 50; i++) {
+        area.appendChild(document.createElement('br'));
+      }
+      area.appendChild(elem);
+      document.body.appendChild(area);
+      let ar = area.getBoundingClientRect();
+      let er = elem.getBoundingClientRect();
+      scrollIfNeeded(area, elem);
+      expect(area.scrollTop).to.be(Math.round(er.bottom - ar.bottom));
+      document.body.removeChild(area);
+    });
+
+    it('should scroll an element down if needed', () => {
+      let area = document.createElement('div');
+      let elem = document.createElement('div');
+      area.style.height = '100px';
+      area.style.overflow = 'auto';
+      elem.textContent = 'visible content';
+      area.appendChild(elem);
+      for (let i = 0; i < 50; i++) {
+        area.appendChild(document.createElement('br'));
+      }
+      document.body.appendChild(area);
+      area.scrollTop = 100;
+      let ar = area.getBoundingClientRect();
+      let er = elem.getBoundingClientRect();
+      scrollIfNeeded(area, elem);
+      expect(area.scrollTop).to.be(Math.round(100 - ar.top + er.top));
+      document.body.removeChild(area);
+    });
+
+    it('should respect the overflow threshold when scrolling up', () => {
+      let area = document.createElement('div');
+      let elem = document.createElement('div');
+      area.style.height = '100px';
+      area.style.overflow = 'auto';
+      elem.textContent = 'visible content';
+      for (let i = 0; i < 50; i++) {
+        area.appendChild(document.createElement('br'));
+      }
+      area.appendChild(elem);
+      document.body.appendChild(area);
+
+      let threshold = 10;
+      let ar = area.getBoundingClientRect();
+      let er = elem.getBoundingClientRect();
+      let goal = Math.round(er.bottom - ar.bottom);
+      area.scrollTop = er.bottom - ar.bottom - threshold + 2;
+      let top = area.scrollTop;
+      scrollIfNeeded(area, elem, threshold);
+      expect(area.scrollTop).to.be(top);
+
+      ar = area.getBoundingClientRect();
+      er = elem.getBoundingClientRect();
+      area.scrollTop = er.bottom - ar.bottom - threshold;
+      top = area.scrollTop;
+      scrollIfNeeded(area, elem, threshold);
+      expect(area.scrollTop).to.be(goal);
+      document.body.removeChild(area);
+    });
+
+    it('should respect the overflow threshold when scrolling down', () => {
+      let area = document.createElement('div');
+      let elem = document.createElement('div');
+      area.style.height = '100px';
+      area.style.overflow = 'auto';
+      elem.textContent = 'visible content';
+      area.appendChild(elem);
+      for (let i = 0; i < 50; i++) {
+        area.appendChild(document.createElement('br'));
+      }
+      document.body.appendChild(area);
+      area.scrollTop = 100;
+
+      let threshold = 10;
+      let ar = area.getBoundingClientRect();
+      let er = elem.getBoundingClientRect();
+      let goal = threshold;
+      area.scrollTop = er.top - ar.top + threshold - 2;
+      let top = area.scrollTop;
+      scrollIfNeeded(area, elem, threshold);
+      expect(area.scrollTop).to.be(top);
+
+      ar = area.getBoundingClientRect();
+      er = elem.getBoundingClientRect();
+      area.scrollTop = er.top - ar.top + threshold;
+      scrollIfNeeded(area, elem, threshold);
+      expect(area.scrollTop).to.be(goal);
+      document.body.removeChild(area);
     });
 
   });
