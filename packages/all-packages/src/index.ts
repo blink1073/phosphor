34e137818a5d5167642ecb30ced96302d753c744
Steven Silvester <steven.silvester@ieee.org>
2017-05-04 15:45:44 -0500

    Reinstate docs build

---
+++ packages/all-packages/src/index.ts
@@ -0,0 +1,38 @@
+/*-----------------------------------------------------------------------------
+| Copyright (c) 2014-2017, PhosphorJS Contributors
+|
+| Distributed under the terms of the BSD 3-Clause License.
+|
+| The full license is in the file LICENSE, distributed with this software.
+|----------------------------------------------------------------------------*/
+
+import * as algorithm from '../../algorithm/src/index';
+import * as application from '../../application/src/index';
+import * as collections from '../../collections/src/index';
+import * as commands from '../../commands/src/index';
+import * as coreutils from '../../coreutils/src/index';
+import * as disposable from '../../disposable/src/index';
+import * as domutils from '../../domutils/src/index';
+import * as dragdrop from '../../dragdrop/src/index';
+import * as keyboard from '../../keyboard/src/index';
+import * as messaging from '../../messaging/src/index';
+import * as properties from '../../properties/src/index';
+import * as signaling from '../../signaling/src/index';
+import * as virtualdom from '../../virtualdom/src/index';
+import * as widgets from '../../widgets/src/index';
+
+export { algorithm,
+         application,
+         collections,
+         commands,
+         coreutils,
+         disposable,
+         domutils,
+         dragdrop,
+         keyboard,
+         messaging,
+         properties,
+         signaling,
+         virtualdom,
+         widgets
+};
